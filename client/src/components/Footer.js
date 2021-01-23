5f26327e8c8d3998ff20493afb041f848b3e38f6
Christopher Clemente <4646283+cmcclemente@users.noreply.github.com>
2021-01-22 23:41:45 -0800

    add bare layout components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer id="footer">
+			<div className="innertube">
+				<p>Footer</p>
+			</div>
+		</footer>
+    )
+}
+
+export default Footer
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header id="header">
+			<div className="innertube">
+				<h1>Header</h1>
+			</div>
+		</header>
+    )
+}
+
+export default Header
