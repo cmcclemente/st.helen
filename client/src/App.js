7280ad74ad6a8098356e157c8ac4cdb72d7e64b5
cagunning <shelfnunny@gmail.com>
2021-01-25 12:04:49 -0800

    Found out how to route to other pages
    if you click the sign up part now it links you to the sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
-
-
-
+import SignUp from './components/SignUp';
+import React from 'react';
 
 
 function App() {
   return (
-    
-    <div className="App">
-      <SearchBar/>
-      <Header /> 
-        <Main />
-      <Footer />
+    <div>
+      <span>
+        <SearchBar/>
+      </span>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            
+            <Header />
+              <Main />
+            <Footer />
+          </div>
+        </Route>
+        <Route
+          exact
+          path="/signup"
+          render={(props) =>
+            <SignUp />
+          } />
+      </Switch>
+    </BrowserRouter>
     </div>
-    
   );
 }
 
