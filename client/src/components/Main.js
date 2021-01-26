fd7b120519f330aa691f03f2aed50fcc3d877fdb
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-01-26 01:38:17 -0800

    Added more structure to main and footer on homepage. header and nav still need css.

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,14 +2,14 @@ const Main = () => {
     return (
         <div className="row"><main>
             <nav id="nav">
-                <div className="innertube">
+                <aside className="innertube">
                     <h3>Left Navigation</h3>
-                </div>
+                </aside>
             </nav>
             <div id="content">
-                <div className="innertube">
+                
                     <h1>Content</h1>
-                </div>
+                
             </div>
         </main>
         </div>
