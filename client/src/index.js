97f0dc1bb591abb000fe31ab7197e5eb585f12db
cagunning <shelfnunny@gmail.com>
2021-01-31 13:55:15 -0800

    Added products + product page
    added a few columns of products in the main, and then a product page when you click any of the products

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './signup.css';
+import './Main.css';
+import './ProductPage.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
