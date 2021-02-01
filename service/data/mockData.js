e6a0335616828847a275693c63ccbd9cae67d0ba
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-01-31 19:20:57 -0800

    Set up the prototype api for the product list

---
+++ service/data/mockData.js
@@ -0,0 +1,6 @@
+exports.productList = [
+    {name: 'a lamp', price: 12.99, available: true},
+    {name: 'a book', price: 7.99, available: true},
+    {name: 'a mouse', price: 20.00, available: true},
+    {name: 'a pain reliever', price: 2.99, available: true},
+]
--- service/index.js
+++ service/index.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (request, response) => {
+
+const productRouter = require('./routes/productRouter');
+
+const logger = (req,res,next)=>{
+    console.log(req.method + ' '+ req.url);
+    return next();
+}
+
+app.use(logger,express.json());
+app.use('/api/v1/products', productRouter);
+
+/*app.get('/', (request, response) => {
     response.send("Hello World!");
-});
+});*/
+
+/*app.get('/api/v1/products', productRouter);*/
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}...`);
+    
+
 })
