7655e0c762c788f0f11e961c1c70d3231242fb5d
Christopher Clemente <4646283+cmcclemente@users.noreply.github.com>
2021-02-12 19:33:45 -0700

    test db oonnection

--- service/index.js
+++ service/index.js
@@ -2,18 +2,29 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 
 const productRouter = require('./routes/productRouter');
 const profileRouter = require('./routes/profileRouter');
 const transactionRouter = require('./routes/transactionRouter');
 
-const logger = (req,res,next)=>{
-    console.log(req.method + ' '+ req.url);
+const mongoURI = 'mongodb+srv://dbAdmin:gTjdXeMCRJTAuiiA@st-helens.prav3.mongodb.net?retryWrites=true&w=majority';
+
+const logger = (req, res, next) => {
+    console.log(req.method + ' ' + req.url);
     return next();
 }
 
-app.use(logger,express.json());
+MongoClient.connect(mongoURI, (err, client) => {
+    if (err) {
+        throw err;
+    }
+    console.log("connected");
+
+})
+
+app.use(logger, express.json());
 app.use(cors());
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/profiles', profileRouter);
@@ -27,6 +38,6 @@ app.use('/api/v1/transactions', transactionRouter);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}...`);
-    
+
 
 })
