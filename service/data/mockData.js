4cd6120df4c06ab85ea5a984633fd08f9cb9a1ea
Jflands23 <75229373+Jflands23@users.noreply.github.com>
2021-03-08 15:27:00 -0800

    removed id from product mockdata

--- service/data/mockData.js
+++ service/data/mockData.js
@@ -1,47 +1,22 @@
 exports.productList = [
     {
-        id: 1,
         name: 'lamp', 
         price: 1200.99, 
         available: true},
     {
-        id:2,
         name: 'book', 
         price: 7.99, 
         available: true},
     {
-        id:3,
         name: 'mouse', 
         price: 20.10, 
         available: true},
     {
-        id:4,
         name: 'pain reliever', 
         price: 2.99, 
         available: true},
     {
-        id:5,
         name: 'phone',
         price: 1000.00,
         available:true},
-    {
-        id:6,
-        name: 'phone',
-        price: 1000.95,
-        available:false},
-    {
-        id:7,
-        name: 'phone',
-        price: 1000.00,
-        available:false},
-    {
-        id:8,
-        name: 'phone',
-        price: 1000.00,
-        available:false},
-    {
-        id:9,
-        name: 'phone',
-        price: 1000.00,
-        available:false},
 ]
